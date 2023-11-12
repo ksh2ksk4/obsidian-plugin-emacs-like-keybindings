574778c2000b31624e786d33a0e3d84a102f2d3e
ksh2ksk4 <github.clicker425@passinbox.com>
2023-11-12 23:39:49 +0900

    Add newline command

--- main.ts
+++ main.ts
@@ -1,7 +1,36 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-export default class MyPlugin extends Plugin {
+export default class EmacsLikeKeybindingsPlugin extends Plugin {
+  private readonly pluginName = 'EmacsLikeKeybindingsPlugin';
+
+  private log(message: string, param: any = null): void {
+    param == null ?
+      console.log(`${this.pluginName}: ${message}`) :
+      console.log(`${this.pluginName}: ${message}`, param);
+  }
+
   async onload() {
+    this.log('onload() - Start');
+
+    this.addCommand({
+      id: 'newline',
+      name: 'Newline',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        this.log('command newline - Start');
+
+        const position = editor.getCursor();
+        this.log('command newline - %o', position);
+        editor.exec('newlineAndIndent');
+
+        this.log('command newline - End');
+      },
+      hotkeys: [{
+        modifiers: ['Ctrl'],
+        key: 'j'
+      }]
+    });
+
+    this.log('onload() - End');
   }
 
   onunload() {
