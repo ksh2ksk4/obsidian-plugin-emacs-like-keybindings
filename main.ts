a4bb8cfab0d51ea7d1466cfde15524176d0ed855
ksh2ksk4 <github.clicker425@passinbox.com>
2023-11-14 00:50:51 +0900

    Add kill-line command

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, EditorPosition, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 export default class EmacsLikeKeybindingsPlugin extends Plugin {
   private readonly pluginName = 'EmacsLikeKeybindingsPlugin';
@@ -23,6 +23,34 @@ export default class EmacsLikeKeybindingsPlugin extends Plugin {
       hotkeys: [{
         modifiers: ['Ctrl'],
         key: 'j'
+      }]
+    });
+
+    this.addCommand({
+      id: 'kill-line',
+      name: 'Kill line',
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        const logPrefix: string = 'command kill-line';
+
+        this.log(`${logPrefix} - Start`);
+
+        const position: EditorPosition = editor.getCursor();
+        this.log(`${logPrefix} - %o`, position);
+        const line: string = editor.getLine(position.line);
+        const remainingText: string = line.slice(0, position.ch);
+        const killedText: string = line.slice(position.ch);
+        this.log(`${logPrefix} - ${remainingText}`);
+        this.log(`${logPrefix} - ${killedText}`);
+        // https://developer.mozilla.org/ja/docs/Web/API/Navigator
+        navigator.clipboard.writeText(killedText);
+        editor.setLine(position.line, remainingText);
+        editor.setCursor(position, position.ch);
+
+        this.log(`${logPrefix} - End`);
+      },
+      hotkeys: [{
+        modifiers: ['Ctrl'],
+        key: 'k'
       }]
     });
 
